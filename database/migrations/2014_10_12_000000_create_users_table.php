46d96c07beb1b1028d1af1d3af141f452ae93284
Mateus Pinheiro <teusbarros@Mateuss-MacBook-Pro.local>
2023-03-28 16:07:32 -0300

    #1 - User migrations restrictions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,15 +16,16 @@ return new class extends Migration
             $table->uuid('uuid');
             $table->string('first_name');
             $table->string('last_name');
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->string('address');
             $table->string('phone_number');
-            $table->boolean('is_marketing');
-            $table->timestamp('last_login_at');
+            $table->boolean('is_marketing')->default(false);
+            $table->timestamp('last_login_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
