e15e357e4101187cbbe7b62e87fac9ab50452659
Mateus Pinheiro <teusbarros@Mateuss-MacBook-Pro.local>
2023-04-02 16:48:29 -0300

    Delete product #14

--- app/Http/Controllers/API/v1/ProductController.php
+++ app/Http/Controllers/API/v1/ProductController.php
@@ -196,4 +196,48 @@ class ProductController extends Controller
     {
         return $this->jsonResponse(new ProductResource($product));
     }
+
+    /**
+     * @OA\Delete(
+     *     tags={"Products"},
+     *     path="/api/v1/product/{uuid}",
+     *     summary="Delete an existing product",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Parameter(
+     *         name="uuid",
+     *         required=true,
+     *         in="path",
+     *         description="",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error"
+     *     ),
+     * )
+     */
+    public function destroy(Product $product): JsonResponse
+    {
+        $product->delete();
+
+        return $this->jsonResponse([]);
+    }
 }
